51ff100ff0f29c4047e965f1e96b10b021c1d209
espacioes <46869922+espacioes@users.noreply.github.com>
2019-01-27 19:26:03 +0100

    Add files via upload

---
+++ php/contacto.php
@@ -0,0 +1,10 @@
+<?php
+	$destino="christian.tc@hotmail.com";
+	$nombre=$_POST["name"];
+	$correo=$_POST["email"];;
+	$mensaje=$_POST["message"];
+	$contenido="Nombre: ".$nombre."\nCorreo: ".$correo."\nMensaje: ".$mensaje;
+	mail($destino,"Contacto", $contenido);
+	header("Location: gracias.html");
+
+?>
